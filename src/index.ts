b44cf94d73003a4ed61c90a1bd306aadce1053bd
Gabriel Trujillo <joaqt23@gmail.com>
2022-01-26 19:23:53 -0500

    Added support for defn files and finished

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
+import createCylinder from 'cylinder';
+
 let diameter: number;
 let depth: number;
 
 diameter = +process.argv[2] || 1;
 depth = +process.argv[3] || 2;
 
-console.log(diameter, depth);
+const cylinder1 = createCylinder(diameter, depth);
+
+console.log('Diameter: %d', cylinder1.diameter);
+console.log('Depth: %d', cylinder1.depth);
+console.log('Round face area: %d', cylinder1.area());
+console.log('Volume: %d', cylinder1.volume());
+
